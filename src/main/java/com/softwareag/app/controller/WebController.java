56ab0592e3281c4a071b5be93d5fb37337331945
jrud <jonas.rudolph@softwareag.com>
2023-09-29 12:03:41 +0200

    welcome, overview and form frontend first draft

--- src/main/java/com/softwareag/app/controller/WebController.java
+++ src/main/java/com/softwareag/app/controller/WebController.java
@@ -5,20 +5,28 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+
 @Controller
 public class WebController {
 
     @GetMapping("/welcome")
     public String welcomeView(Model model) {
         // Add any necessary data to the model for rendering the view
-        model.addAttribute("pageTitle", "Meine Ansicht");
+        model.addAttribute("pageTitle", "AAS Builder");
         return "welcome"; // This corresponds to a view named "view.html" in your templates folder
     }
 
 
     @GetMapping("/aas/overview")
-    public String showOverview(){
+    public String showOverview(Model model){
+        model.addAttribute("pageTitle", "AAS Overview");
         return "overview";
+    }
+
+    @GetMapping("/aas/form")
+    public String showOver(Model model){
+        model.addAttribute("pageTitle", "AAS Overview");
+        return "aas_form";
     }
 
 
