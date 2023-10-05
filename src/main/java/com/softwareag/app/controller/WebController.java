550c08f49fe6df79be3c35a9dc162a0a22f1c1f0
jrud <jonas.rudolph@softwareag.com>
2023-10-05 11:13:02 +0200

    accordion is working

--- src/main/java/com/softwareag/app/controller/WebController.java
+++ src/main/java/com/softwareag/app/controller/WebController.java
@@ -3,7 +3,9 @@ package com.softwareag.app.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
@@ -24,10 +26,41 @@ public class WebController {
     }
 
     @GetMapping("/aas/form")
-    public String showOver(Model model){
-        model.addAttribute("pageTitle", "AAS Overview");
+    public String showForm(Model model){
+        model.addAttribute("pageTitle", "AAS Configurator");
         return "aas_form";
     }
+
+
+
+    @PostMapping("/aas/submission")
+    public String submussion(
+        @RequestParam("assetIDshort") String assetIDshort,
+        @RequestParam("assetID") String assetID,
+        @RequestParam("PCFCalculationMethod") String PCFCalculationMethod,
+        @RequestParam("PCFCO2eq") double PCFCO2eq,
+        @RequestParam("PCFQuantityOfMeasureForCalculation") double PCFQuantityOfMeasureForCalculation,
+        @RequestParam("PCFLiveCyclePhase") String PCFLiveCyclePhase,
+        @RequestParam("TCFCalculationMethod") String TCFCalculationMethod,
+        @RequestParam("TCFCO2eq") double TCFCO2eq,
+        @RequestParam("TCFReferenceValueForCalculation") String TCFReferenceValueForCalculation,
+        @RequestParam("TCFQuantityOfMeasureForCalculation") String TCFQuantityOfMeasureForCalculation,
+        @RequestParam("TCFLiveCyclePhase") String TCFLiveCyclePhase
+
+    ){
+        System.out.println(assetIDshort);
+        System.out.println(assetID);
+        System.out.println(PCFCalculationMethod);
+        System.out.println(PCFQuantityOfMeasureForCalculation);
+        System.out.println(PCFLiveCyclePhase);
+        System.out.println(TCFCalculationMethod);
+        System.out.println(TCFCO2eq);
+        System.out.println(TCFReferenceValueForCalculation);
+        System.out.println(TCFQuantityOfMeasureForCalculation);
+        System.out.println(TCFLiveCyclePhase);
+        return "redirect:/aas/overview";
+    }
+
 
 
 
